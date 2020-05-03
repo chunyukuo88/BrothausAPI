41ae797dc6e675d3844ea3be9b011fa8f0990c39
chunyukuo88 <alexgochenour@gmail.com>
2020-05-03 00:16:36 -0400

    Deleted the extraneous handler.js in the root directory.

--- wooblerAPI/lambdas/getFactHandler.test.js
+++ wooblerAPI/lambdas/getFactHandler.test.js
@@ -4,13 +4,22 @@ describe('getFactHandler.js: ', ()=>{
     describe('When passed a valid http request, ', () => {
         it('return the corresponding fact.', async () => {
             const httpRequest = {
-                'pathParameters': {
-                    'ID': '1'
-                }
+                'pathParameters': { 'ID': '1'}
             };
             const response = await handler(httpRequest);
             console.log(response);
             expect(response.statusCode).toBe(200);
         });
     });
+    describe('When passed an invalid http request, ', () => {
+        it('returns an error code 400.', async () => {
+            const httpRequest = {
+                'pathParameters': { 'ID': 'Chuck Norris'}
+            };
+            const response = await handler(httpRequest);
+            console.log(response);
+            expect(response.statusCode).toBe(400);
+        });
+    });
+
 });
